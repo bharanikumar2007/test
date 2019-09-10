3cbfe26ce93989b55829ac270575ada36482e2db
gbharani <gbharani@mnibharanig.modeln.com>
2019-09-10 09:11:56 +0530

    Added Authentication

--- src/com/test/gets/GetRepositories.java
+++ src/com/test/gets/GetRepositories.java
@@ -12,12 +12,10 @@ public class GetRepositories {
 	 * @throws IOException
 	 */
 	public static void main(String[] args) throws IOException {
-		final String user = "bharanikumar2007";
-		final String format = "{0}) {1}- created on {2}";
-		int count = 1;
-		RepositoryService service = new RepositoryService();
-		for (Repository repo : service.getRepositories(user))
-			System.out.println(MessageFormat.format(format, count++,
-					repo.getName(), repo.getCreatedAt()));
+		final String userName = "bharanikumar2007";
+		RepositoryService repService = new RepositoryService();
+		repService.getClient().setCredentials("bharanikumar2007", "karuna@143");
+		for (Repository repo : repService.getRepositories())
+			System.out.println(repo.getName() + " @" + repo.getCreatedAt());
 	}
 }
